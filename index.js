93b92eb505f175aa9cf3d9a7ae21abeb98b8d086
Gosu <56687029+HereIamGosu@users.noreply.github.com>
2024-12-22 02:44:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,26 +4,30 @@ const path = require('path');
 
 const app = express();
 
-// Подключаем статические файлы
-app.use(express.static(path.join(__dirname, 'public')));
+// Подключаем статические файлы (путь 'public' можно сразу передавать в метод express.static)
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 // Главная страница
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
-// Маршрут для генерации конфига
+// Маршрут для генерации конфигурации
 app.get('/warp', async (req, res) => {
     try {
+        // Получение ссылки на конфиг
         const content = await getWarpConfigLink();
+        
         if (content) {
             res.json({ success: true, content });
         } else {
+            // Уточняющее сообщение для клиента
             res.status(500).json({ success: false, message: 'Не удалось сгенерировать конфиг.' });
         }
     } catch (error) {
         console.error('Ошибка при обработке запроса:', error);
-        res.status(500).json({ success: false, message: 'Произошла ошибка на сервере.' });
+        // Обработка ошибок с более информативным сообщением
+        res.status(500).json({ success: false, message: 'Произошла ошибка на сервере. Попробуйте позже.' });
     }
 });
 
